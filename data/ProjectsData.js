b9c6adf82492c1b9202009c9155847fe4e491f12
mhmdarfiansyahp <mhmdarfiansyahp@gmail.com>
2026-07-28 16:37:34 +0700

    update project

--- data/ProjectsData.js
+++ data/ProjectsData.js
@@ -35,72 +35,72 @@ const projects = [
       { label: "Frontend", url: "https://github.com/mhmdarfiansyahp/PotekGas-Fe.git" },
       { label: "Backend", url: "https://github.com/mhmdarfiansyahp/PotekGas-Be.git" }
     ]
-  },
-  {
-    id: 3,
-    title: "Internak",
-    description: [
-      "Developed an Android application integrated with SQL Server.",
-      "Automated livestock barn management and monitoring."
-    ],
-    image: "/assets/images/Internak.png",
-    screenshots: [
-      "/assets/images/Internak.png",
-      "/assets/images/Internak-2.png",
-      "/assets/images/Internak-3.png",
-    ],
-    tech: ["Java", "Android", "Spring Boot", "SQL Server"],
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/mhmdarfiansyahp/Internak.git",
-      },
-      {
-        label: "Backend",
-        url: "https://github.com/mhmdarfiansyahp/Internak-BE.git",
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "HeroApp",
-    description: [
-      "Developed a mobile application for equipment lending and maintenance.",
-      "Improved practicum efficiency for students and lecturers."
-    ],
-    image: "/assets/images/HeroApp.png",
-    screenshots: [
-      "/assets/images/HeroApp.png",
-      "/assets/images/HeroApp-2.png",
-      "/assets/images/HeroApp-3.png",
-    ],
-    tech: ["Java", "Android", "PHP", "MySQL"],
-    github: [
-      {
-        label: "Frontend",
-        url: "https://github.com/DaffaAbiyyu29/HEROApp.git",
-      },
-      {
-        label: "Backend",
-        url: "https://github.com/salsans/HeroApp_BE.git",
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "Dashboard 5K2S", // 📌 Proyek Awal
-    description: [
-      "A web platform designed to make it easier for users to view, create, display, and filter data for each student class.",
-      "Implemented histogram charts and assessment history features for each user."
-    ],
-    image: "/assets/images/Dashboard5K2S.png",
-    screenshots: [
-      "/assets/images/Dashboard5K2S.png",
-      "/assets/images/Dashboard5K2S-2.png",
-      "/assets/images/Dashboard5K2S-3.png",
-    ],
-    tech: ["CodeIgniter", "PHP", "JavaScript", "MySQL"],
-    github: "https://github.com/mhmdarfiansyahp/Dashboard5K2S.git",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Internak",
+  //   description: [
+  //     "Developed an Android application integrated with SQL Server.",
+  //     "Automated livestock barn management and monitoring."
+  //   ],
+  //   image: "/assets/images/Internak.png",
+  //   screenshots: [
+  //     "/assets/images/Internak.png",
+  //     "/assets/images/Internak-2.png",
+  //     "/assets/images/Internak-3.png",
+  //   ],
+  //   tech: ["Java", "Android", "Spring Boot", "SQL Server"],
+  //   github: [
+  //     {
+  //       label: "Frontend",
+  //       url: "https://github.com/mhmdarfiansyahp/Internak.git",
+  //     },
+  //     {
+  //       label: "Backend",
+  //       url: "https://github.com/mhmdarfiansyahp/Internak-BE.git",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   title: "HeroApp",
+  //   description: [
+  //     "Developed a mobile application for equipment lending and maintenance.",
+  //     "Improved practicum efficiency for students and lecturers."
+  //   ],
+  //   image: "/assets/images/HeroApp.png",
+  //   screenshots: [
+  //     "/assets/images/HeroApp.png",
+  //     "/assets/images/HeroApp-2.png",
+  //     "/assets/images/HeroApp-3.png",
+  //   ],
+  //   tech: ["Java", "Android", "PHP", "MySQL"],
+  //   github: [
+  //     {
+  //       label: "Frontend",
+  //       url: "https://github.com/DaffaAbiyyu29/HEROApp.git",
+  //     },
+  //     {
+  //       label: "Backend",
+  //       url: "https://github.com/salsans/HeroApp_BE.git",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 5,
+  //   title: "Dashboard 5K2S",
+  //   description: [
+  //     "A web platform designed to make it easier for users to view, create, display, and filter data for each student class.",
+  //     "Implemented histogram charts and assessment history features for each user."
+  //   ],
+  //   image: "/assets/images/Dashboard5K2S.png",
+  //   screenshots: [
+  //     "/assets/images/Dashboard5K2S.png",
+  //     "/assets/images/Dashboard5K2S-2.png",
+  //     "/assets/images/Dashboard5K2S-3.png",
+  //   ],
+  //   tech: ["CodeIgniter", "PHP", "JavaScript", "MySQL"],
+  //   github: "https://github.com/mhmdarfiansyahp/Dashboard5K2S.git",
   }
 ];
 
